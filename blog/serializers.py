5dfc1f299075360b8dab5c7ab422c9cf9af5589f
ArturKamnev <kamnevartur28@gmail.com>
2026-07-14 22:08:29 +0600

    Контрольная работа

--- blog/serializers.py
+++ blog/serializers.py
@@ -3,17 +3,59 @@ from rest_framework import serializers
 from .models import Post, Comment
 
 class PostSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Post
-        fields = ['author', 'title', 'created_at', 'is_published']
+        fields = [
+            'id',
+            'author',
+            'title',
+            'body',
+            'created_at',
+            'updated_at',
+            'is_published',
+        ]
+
 
 class PostDetailSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = [
+            'id',
+            'author',
+            'title',
+            'body',
+            'created_at',
+            'updated_at',
+            'is_published',
+        ]
+
 
 class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+    author = serializers.PrimaryKeyRelatedField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+    is_approved = serializers.BooleanField(read_only=True)
+
     class Meta:
         model = Comment
-        fields = ['post', 'author', 'body', 'created_at', 'is_approved', 'updated_at']
-    
+        fields = [
+            'id',
+            'post',
+            'author',
+            'body',
+            'created_at',
+            'updated_at',
+            'is_approved',
+        ]
